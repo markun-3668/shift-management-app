a811f08be9003abaa172340245cdae4a92e46584
markun-3668 <markun.3668@icloud.com>
2025-06-06 18:56:08 +0900

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
+  output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/shift-management-app' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/shift-management-app' : '',
+  }
 };
-export default nextConfig;
+
+module.exports = nextConfig;
